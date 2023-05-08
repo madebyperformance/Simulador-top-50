71ef5469e6f1cab907443d09601ba2a8a9be4adc
madebyperformance <124921985+madebyperformance@users.noreply.github.com>
2023-05-08 17:26:39 -0300

    Update simulador50.py

--- simulador50.py
+++ simulador50.py
@@ -86,7 +86,7 @@ if st.button('Calcular nova posição'):
         rank_antigo = df.loc[df['KPI4'] == ranking, 'KPI4'].iloc[0]
         rank_atual = df.loc[df['KPI4'] == ranking, 'KPI42'].iloc[0] # Selecionando o primeiro valor retornado
         st.write("<span style='font-family: Barlow; color: grey;font-size: 14px;'>As posições são calculadas levando em conta que todos os Assessores manterão e média de Incremento, Faturamento e NPS. É natural que a projeção não bata na vírgula mas trará uma posição bem aproximada..</span>", unsafe_allow_html=True) 
-        st.write(f"<span style='font-family: Barlow; color: black;font-size: 20px;'>Sua posição antiga era: </span><span style='color: #9966ff'>{rank_antigo}</span>", unsafe_allow_html=True)
+        st.write(f"<span style='font-family: Barlow; color: black;font-size: 20px;'>Sua posição antiga era: </span><span style='font-family: Barlow; color: #9966ff;font-size: 20px;'>{rank_antigo}</span>", unsafe_allow_html=True)
         st.write(f"<span style='font-family: Barlow; color: black;font-size: 20px;'>Sua nova posição será: {rank_atual}</span>", unsafe_allow_html=True) 
         if rank_antigo <=50:
             if rank_atual > 50:
